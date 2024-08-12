e02dae8cf8dee2d5f4bd19519a5d2b5203e6b742
enias-oliveira <enias.felipe@gmail.com>
2024-08-12 17:08:23 -0300

    feat(wip): adds event components to app and creates use app hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useApp } from "./use-app";
+
 export const App = () => {
+  const { events } = useApp();
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
-
+    <div>
+      {events.map((sportEvent) => (
+        <div key={sportEvent.id}>
+          <div>
+            <span>{sportEvent.name}</span>
+          </div>
+          {sportEvent.markets.map((market) => (
+            <div key={market.id}>
+              {market.name}
+              <div className="flex">
+                {market.selections.map((selection) => (
+                  <button key={selection.id} className="flex flex-col">
+                    <div>{selection.name}</div>
+                    <div>{selection.price}</div>
+                  </button>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
