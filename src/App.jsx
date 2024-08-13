0ecee07837c2b6826e59b6e1835026f799a8a1e1
enias-oliveira <enias.felipe@gmail.com>
2024-08-13 17:29:19 -0300

    feat(wip): hides event with no market

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,34 @@ export const App = () => {
 
   return (
     <div>
-      {events.map((sportEvent) => (
-        <div key={sportEvent.id}>
-          <div>
-            <span>{sportEvent.name}</span>
-          </div>
-          {sportEvent.markets.map((market) => (
-            <div key={market.id}>
-              {market.name}
-              <div className="flex">
-                {market.selections.map((selection) => (
-                  <button key={selection.id} className="flex flex-col">
-                    <div>{selection.name}</div>
-                    <div>{selection.price}</div>
-                  </button>
+      <div>
+        {events.map(
+          (sportEvent) =>
+            sportEvent.markets[0] && (
+              <div key={sportEvent.id}>
+                <div>
+                  <span>{sportEvent.name}</span>
+                </div>
+                {sportEvent.markets.map((market) => (
+                  <div key={market.id}>
+                    {market.name}
+                    <div className="flex">
+                      {market.selections.map((selection) => (
+                        <button key={selection.id} className="flex flex-col">
+                          <div>{selection.name}</div>
+                          <div>{selection.price}</div>
+                        </button>
+                      ))}
+                    </div>
+                  </div>
                 ))}
               </div>
-            </div>
-          ))}
-        </div>
-      ))}
+            )
+        )}
+      </div>
+      <div>
+        <div>Selections</div>
+      </div>
     </div>
   );
 };
