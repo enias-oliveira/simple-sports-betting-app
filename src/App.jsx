3da0cfd2db74735c28952902d7c3ca6f0fed9d4a
enias-oliveira <enias.felipe@gmail.com>
2024-08-17 10:35:37 -0300

    feat: adds styles to markets

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,26 @@ export const App = () => {
   const { events, selections, handleAddSelection } = useApp();
 
   return (
-    <div className="flex flex-col items-center justify-around gap-6 bg-green-400 pt-4 px2">
+    <div className="flex flex-col items-center justify-around gap-6  pt-4 px2">
       <div className="flex flex-col items-center justify-around gap-4">
         {events.map(
           (sportEvent) =>
             sportEvent.markets[0] && (
               <div key={sportEvent.id}>
-                <div className="border-solid border-2 border-gray-500 p-4">
+                <div className="border-solid border border-gray-500 p-4">
                   <span>{sportEvent.name}</span>
                 </div>
-                <div className="border-solid border-2 border-gray-500">
+
+                <div className="border-solid border border-gray-500">
                   {sportEvent.markets.map((market) => (
                     <div key={market.id} className="border-2 border-gray-500">
                       <div className="font-thin">{market.name}</div>
-                      <div className="flex justify-evenly">
+
+                      <div className="flex justify-between gap-8 p-2">
                         {market.selections.map((selection) => (
                           <button
                             key={selection.id}
-                            className="flex flex-col border rounded p-1"
+                            className="flex-1 flex-col border rounded text-[0.5rem]"
                             onClick={() => handleAddSelection(selection)}
                           >
                             <div>{selection.name}</div>
